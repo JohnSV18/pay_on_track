09eba362bf66b39fa0431588f2ff7640e05be84d
John Marcos <johnsaguay@gmail.com>
2022-01-31 19:37:56 -0800

    disabled mailer

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,27 +1,27 @@
-// require our mailgun dependencies
-const nodemailer = require('nodemailer');
-const mg = require('nodemailer-mailgun-transport');
+// // require our mailgun dependencies
+// const nodemailer = require('nodemailer');
+// const mg = require('nodemailer-mailgun-transport');
 
-// auth with our mailgun API key and domain
-const auth = {
-  auth: {
-    api_key: process.env.MAILGUN_API_KEY,
-    domain: process.env.EMAIL_DOMAIN
-  }
-}
-// create a mailer
-const nodemailerMailgun = nodemailer.createTransport(mg(auth));
+// // auth with our mailgun API key and domain
+// const auth = {
+//   auth: {
+//     api_key: process.env.MAILGUN_API_KEY,
+//     domain: process.env.EMAIL_DOMAIN
+//   }
+// }
+// // create a mailer
+// const nodemailerMailgun = nodemailer.createTransport(mg(auth));
 
-// export our send mail function
-module.exports.sendMail = (data) => {
-    nodemailerMailgun.sendMail({
-        from: 'no-reply@example.com',
-        to: 'johnsaguay@gmail.com', // An array if you have multiple recipients.
-        subject: 'Reminder to pay all these bills!!',
-        template: {
-            name: './views/email.hbs',
-            engine: 'handlebars',
-            context: data
-        }
-    })
-}
+// // export our send mail function
+// module.exports.sendMail = (data) => {
+//     nodemailerMailgun.sendMail({
+//         from: 'no-reply@example.com',
+//         to: 'johnsaguay@gmail.com', // An array if you have multiple recipients.
+//         subject: 'Reminder to pay all these bills!!',
+//         template: {
+//             name: './views/email.hbs',
+//             engine: 'handlebars',
+//             context: data
+//         }
+//     })
+// }
