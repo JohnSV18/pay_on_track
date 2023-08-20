3721c1cd4493310992ffac269c16501e755db23e
John Marcos <johnsaguay@gmail.com>
2023-08-20 18:40:07 -0400

    added if statement

--- config/db.config.js
+++ config/db.config.js
@@ -2,17 +2,42 @@ const mongoose = require("mongoose");
 assert = require("assert")
 require('dotenv').config();
 
-const uri = process.env.MONGODB_URI
-mongoose.connect(
-  uri,
-  {
-    useNewUrlParser: true, useUnifiedTopology: true
-  },
-  (err) => {
-    assert.equal(null, err);
-    console.log('Connected succesfully to database');
-  }
-);
+
+if (process.env.NODE_ENV === "development") {
+
+  const uri = 'mongodb://localhost:27017/plan-itdata';
+  mongoose.Promise = global.Promise;
+  mongoose.connect(
+    uri, 
+    { 
+      useNewUrlParser: true, useUnifiedTopology: true
+    })
+    .then(() => {
+      console.log("Connected to database")
+    })
+    .catch((err) => {
+      console.log("Cannot connect to database", err)
+      process.exit();
+    })
+        
+} else if (process.env.NODE_ENV === "production") {
+
+  const uri = process.env.MONGODB_URI;
+  mongoose.Promise = global.Promise;
+  mongoose.connect(
+    uri, 
+    { 
+      useNewUrlParser: true, useUnifiedTopology: true
+    })
+    .then(() => {
+      console.log("Connected to database")
+    })
+    .catch((err) => {
+      console.log("Cannot connect to database", err)
+      process.exit();
+    })
+}
+
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 mongoose.set('debug', true);
 
