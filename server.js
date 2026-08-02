ba4b0e202477e47c69310e870d62fe478d065665
John S <johnsaguay@gmail.com>
2026-08-01 22:08:44 -0400

    Seperated and restructured css files for hbs views

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
-      mongoUrl: process.env.MONGODB_URI,
+      mongoUrl: process.env.MONGODB_LOCAL,
       touchAfter: 24 * 3600
     }),
     cookie: {
